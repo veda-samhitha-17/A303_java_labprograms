5e1909fd1797a25339caef40e6d0f95b2672de82
veda samhitha <11249a303@kanchiuniv.ac.in>
2026-08-13 15:37:36 +0530

    Add files via upload

---
+++ Feedback.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Feedback {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double[] ratings = new double[5];
+        double sum = 0;
+
+        System.out.println("Enter 5 feedback ratings:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Rating " + (i + 1) + ": ");
+            ratings[i] = sc.nextDouble();
+            sum = sum + ratings[i];
+        }
+
+        double average = sum / 5;
+
+        System.out.println("\nAverage Rating: " + average);
+
+        sc.close();
+    }
+}
